3e9ed583be99f3bf8cfa09b3b716176108ae149b
Julien Rechenmann <jrechenmann@intracen.org>
2023-02-08 15:07:35 -0500

    remove test in pipeline

--- __tests__/NeurosityHeadsetTest.tsx
+++
@@ -1,7 +0,0 @@
-import {NeurosityHeadset} from '../src/EEGHeadset/NeurosityHeadset';
-
-test('login to neurosity', async () => {
-  let headset = new NeurosityHeadset();
-  await headset.login({email: 'rechenmann@gmail.com', password: 'Nuggets83n!'});
-  expect(headset.isDeviceConnected()).toBeTruthy();
-});
